31057449b95f71687c178738a38d4bc01986fd0e
Nathan Munis <nathanmunis.signal@gmail.com>
2020-05-17 17:01:23 -0300

    Inserção de um menu abaixo do hearder

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import logo from  './../../Assets/Pokestore.png'
+import fire from  './../../Assets/fire.jpg'
+import water from  './../../Assets/water.jpg'
+import ghost from  './../../Assets/ghost.jpg'
+import ground from  './../../Assets/ground.jpg'
+import './style.css'
 export default class Home extends React.Component {
 
     tipoPokemon = (tipo) =>{
         var backgroundGradient= ''
         switch (tipo) {
             case 'Fogo':
-                backgroundGradient = 'linear-gradient(86deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 0%, rgba(250,72,72,1) 50%, rgba(252,73,80,1) 100%)';
+                backgroundGradient = '#ff4040';
                 
             break;
             case 'Agua':
@@ -25,26 +30,35 @@ export default class Home extends React.Component {
             alignItems:'center',
             justifyContent: 'space-around'
         }
-        
+       
+
         return div
     }
     render() {
         return (
-            <div style={this.tipoPokemon('Fogo')}>
-                <div className='logo'>
+            <>
+                <div style={this.tipoPokemon('Fogo')}>
+                    <div className='logo'>
+                        <Link to='/'>
+                            <img src={logo} width="170px" height="50px" alt="Logo da loja" />
+                        </Link>
+                    </div>
+                </div>
+                <div className="content-img-menu">
                     <Link to='/'>
-                        <img src={logo} width="170px" height="50px" alt="Logo da loja" />
+                        <img src={fire}   className="img-menu" alt="type fire"/>
+                    </Link>
+                    <Link to='/'>
+                        <img src={water}  className="img-menu" alt="type water"/>
+                    </Link>
+                    <Link to='/'>
+                        <img src={ghost}  className="img-menu" alt="type ghost"/>
+                    </Link>
+                    <Link to='/'>
+                        <img src={ground} className="img-menu"  alt="type ground"/>
                     </Link>
                 </div>
-                <div className="itens-menu">
-                    <Link to='/'>
-                        <p>Fogo</p>
-                    </Link>
-                    <Link to='/'>
-                        <p>Água</p>
-                    </Link>
-                </div>
-            </div>
+            </>
         )
     }
 
