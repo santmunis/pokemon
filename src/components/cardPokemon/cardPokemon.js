bf55ef2e4df456d9b547fdcf6dcfbb13dbb64ad2
Nathan Munis <nathanmunis.signal@gmail.com>
2020-05-17 16:59:14 -0300

    Refatoração layout

--- src/components/cardPokemon/cardPokemon.js
+++ src/components/cardPokemon/cardPokemon.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Pagination } from '@material-ui/lab';
+import Pagination from './../pagination/pagination'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import { addCarrinho }
     from './../../store/action'
@@ -8,42 +8,30 @@ import { connect } from 'react-redux'
 import './style.css'
 
 class CardPokemon extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          currentPage: 1,
-          todosPerPage: 15,
-          
-        };
-      }
-
-      handleClick = (event, value) => {
-        this.setState({
-          currentPage: Number(value)
-        });
-      }
       
     render() {
-        const { currentPage, todosPerPage } = this.state;
 
         // Lógica Paginação
-        const indexOfLastTodo = currentPage * todosPerPage;
-        const indexOfFirstTodo = indexOfLastTodo - todosPerPage;
+        const indexOfLastTodo = this.props.pagination.currentPage * this.props.pagination.todosPerPage;
+        const indexOfFirstTodo = indexOfLastTodo - this.props.pagination.todosPerPage;
         const currentTodos = this.props.pokemon.filter(this.props.campoPesquisa).slice(indexOfFirstTodo, indexOfLastTodo);
-        const pageNumbers = Math.ceil(this.props.pokemon.filter(this.props.campoPesquisa).length / todosPerPage)
-            if(Math.ceil(this.props.pokemon.filter(this.props.campoPesquisa).length / todosPerPage) < Math.ceil(this.props.pokemon.length / todosPerPage) ){
-                if(currentPage !== 1){
-                    this.setState({currentPage: 1})
-                }
+        const pageNumbers = Math.ceil(this.props.pokemon.filter(this.props.campoPesquisa).length / this.props.pagination.todosPerPage)
+            if(Math.ceil(this.props.pokemon.filter(this.props.campoPesquisa).length / this.props.pagination.todosPerPage) < Math.ceil(this.props.pokemon.length / this.props.pagination.todosPerPage) ){
+               
             }
         return (
             <>
+                <div>
+                    <Pagination pageNumbers={pageNumbers}/>
+                </div>
                 <div className="content-pokemon">    
                 {
                     currentTodos.map(pokemon =>{
                         return(
                                 <div className="card-pokemon" key={pokemon.name}>
-                                    <img src={pokemon.url.front_default} className="pokemon-img" alt="Pokemon Imagem Frontal"/>
+                                    <div className="div-img-pokemon">
+                                        <img src={pokemon.url.front_default} className="pokemon-img" alt="Pokemon Imagem Frontal"/>
+                                    </div>
                                     <p className="title"> {pokemon.name}</p>
                                     <p className="preco">R$ {pokemon.preco}</p>
                                     <div className="button" onClick={()=>{this.props.addPokemonCarrinho(pokemon)}}>
@@ -56,15 +44,26 @@ class CardPokemon extends React.Component {
                     })
                 }
                 </div>
+                <div>
+                    <Pagination pageNumbers={pageNumbers}/>
+                </div>
+                {
+                    /*
                 <div className="pagination">
                     <Pagination count={pageNumbers} onChange={this.handleClick} />
                 </div>
+                */
+                }
             </>
         )
     }
 
 }
-
+const mapStateToProps = state => {
+    return {
+        pagination: state.pagination
+    }
+}
 const mapDispatchToProps = dispatch => {
     return {
         addPokemonCarrinho: (pokemon) => {
@@ -73,4 +72,4 @@ const mapDispatchToProps = dispatch => {
     }
 }
 
-export default connect(null, mapDispatchToProps)(CardPokemon)
+export default connect(mapStateToProps, mapDispatchToProps)(CardPokemon)
