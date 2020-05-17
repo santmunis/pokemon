14c693e72fb3a9d9a81bb82066199d69bf889077
Nathan Munis <nathanmunis.signal@gmail.com>
2020-05-17 19:58:43 -0300

    Armazenamento carrinho em session

--- src/store/Reducers/reducersCarrinho.js
+++ src/store/Reducers/reducersCarrinho.js
@@ -1,9 +1,21 @@
 import { ADD_CARRINHO, LISTA_CARRINHO, DELETE_CARRINHO } from '../types'
 
-export default function reducerEditar(state = [], action) {
+let initialState
+if(localStorage.getItem("pokemon/carrinhoCompras") === undefined || localStorage.getItem("pokemon/carrinhoCompras") === null){
+    console.log("entrou")
+    localStorage.setItem('pokemon/carrinhoCompras', "[]")
+}else{
+    initialState = JSON.parse(localStorage.getItem('pokemon/carrinhoCompras'))
+}
+   
+
+
+export default function reducerEditar(state = initialState, action) {
    switch (action.type) {
       case LISTA_CARRINHO: return state
-      case ADD_CARRINHO: return [...state, action.pokemons]
+      case ADD_CARRINHO: 
+      localStorage.setItem('pokemon/carrinhoCompras', JSON.stringify([...state, action.pokemons]))
+      return [...state, action.pokemons]
       case DELETE_CARRINHO: 
       //vetor com index a serem excluidos  
       var deletar = action.pokemonsDeletado
@@ -11,11 +23,14 @@ export default function reducerEditar(state = [], action) {
         for (var i = deletar.length -1; i >= 0; i--)
             state[deletar[i]] = " ";
         //Filtra os valores vazios
-            return state.filter(element =>{
+            var novoState = state.filter(element =>{
                 if(element !== " "){
                     return element
                 }
             })
+            localStorage.removeItem('pokemon/carrinhoCompras')
+            localStorage.setItem('pokemon/carrinhoCompras', JSON.stringify(novoState))
+        return novoState
       default: return state
    }
 }
